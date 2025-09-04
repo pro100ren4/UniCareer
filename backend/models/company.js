03844f463030e7966310dc7ecd657783f7443424
pro100ren4 <pro100ren4@gmail.com>
2025-09-04 16:16:17 +0000

    feat: Добавлена модель компании

---
+++ backend/models/company.js
@@ -0,0 +1,52 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Company extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Company.hasOne(models.User, 
+        {
+          foreignKey: 'id',
+          as: 'user',
+          onDelete: 'CASCADE',
+        }
+      )
+    }
+  }
+  Company.init({
+    user_id: { 
+      primaryKey: true,
+      type: DataTypes.NUMBER,
+    },
+    name: { 
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    website: { 
+      type: DataTypes.STRING,
+    },
+    description: { 
+      type: DataTypes.TEXT,
+    },
+    verified: {
+      defaultValue: false, 
+      type: DataTypes.BOOLEAN,
+    },
+    contact_person: { 
+      type: DataTypes.STRING,
+    },
+    logo_path: { 
+      type: DataTypes.TEXT
+    },
+  }, {
+    sequelize,
+    modelName: 'Company',
+  });
+  return Company;
+};
