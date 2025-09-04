24f9ee8e4b846cfa20eda16dc9876019d8ca9312
pro100ren4 <pro100ren4@gmail.com>
2025-09-04 22:39:06 +0000

    feat: Добавлена модель тэга студента

---
+++ backend/migrations/20250904171913-create-student-tag.js
@@ -0,0 +1,33 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('StudentTags', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      student_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      tag_id: {        
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('StudentTags');
+  }
+};
---
+++ backend/models/studenttag.js
@@ -0,0 +1,43 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class StudentTag extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      StudentTag.belongsTo(models.Student, 
+        {
+          foreignKey: 'student_id',
+          as: 'student',
+          onDelete: 'CASCADE'
+        }
+      )
+      StudentTag.belongsTo(models.Tag, 
+        {
+          foreignKey: 'tag_id',
+          as: 'tag',
+          onDelete: 'CASCADE'
+        }
+      )
+    }
+  }
+  StudentTag.init({
+    student_id: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    tag_id: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+  }, {
+    sequelize,
+    modelName: 'StudentTag',
+  });
+  return StudentTag;
+};
