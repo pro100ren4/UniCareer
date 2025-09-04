a15ccdc37962ae7e2285dad569ef8dcf69547d42
pro100ren4 <pro100ren4@gmail.com>
2025-09-04 15:04:43 +0000

    feat: Сделана заготовка под приложение

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
+
+const app = express()
+
+
+// Начальная конфигурация
+
+// HTTP загловки для безопасности
+app.use(helmet())
+// Логирование запросов к backend
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    res.send("<h1>In Development</h1>")
+})
+
+app.listen(5050, (err) => {
+    if (err) {
+        console.error("Server start failed! ", err);
+    } else {
+        console.log("Server started")
+        console.log("http://localhost:5050")
+    }
+})
